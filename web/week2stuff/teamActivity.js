50b42937fae101802c4c178b64e7c4a58071ed0e
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-14 16:53:11 -0700

    modified team activity to final state

--- web/week2stuff/teamActivity.js
+++ web/week2stuff/teamActivity.js
@@ -1,6 +1,6 @@
 function changeColor(){
     //inputColor = document.getElementById("color").value;
-    //document.getElementById("div1").style.backgroundColor = inputColor;
+    //document.getElementById("div1").style.backgroundColor = inputColor;backgroundColor
     inputColor = $('#color').val();
     $('#div1').css('backgroundColor', inputColor);
 }
@@ -9,13 +9,9 @@ function clicked(){
     alert("Clicked");
 }
 
-function moveMe() {
-    $(document).ready(function() {
-        $("p").click(function(){
-            $(this).changeColor();
-        });
-    });
-}
+// $("#toggleJ").click(function(){
+//       $("#div3").fadeOut();
+//     });   
 
 
 
