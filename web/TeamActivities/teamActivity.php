c3c917acd8b79e71897a5acf966de7c888583ca8
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-21 16:24:07 -0700

    team activity stuff

--- web/TeamActivities/teamActivity.php
+++ web/TeamActivities/teamActivity.php
@@ -1,18 +1,19 @@
 <?php
 //get post data
+$isTrue   = true;
 $email    = htmlspecialchars($_POST['email']);
 $password = htmlspecialchars($_POST['password']);
 $major    = htmlspecialchars($_POST['major']);
 $name     = htmlspecialchars($_POST['name']);
 $comments = htmlspecialchars($_POST['comments']);
-$continent = isset($_POST['continent']) ? htmlspecialchars($_POST['continent']) : array();
+$continent = isset($_POST['continent']) ? htmlspecialchars($_POST['continent']) : $isTrue = false ;
 
 //display the data data
 echo "Name: $name <br>";
 echo "<a href='mailto:$email'>$email</a><br>"; //making this a link
 echo "Major: $major <br>";
 echo "Comments: $comments <br>";
-if ($continent) {
+if ($isTrue) {
     echo "You have selected the continents: <br>"; 
     foreach($continent as $value) {
         echo " " . $value . "<br>";
