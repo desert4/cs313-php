989c034dae0143e4329cc62b94cca526e59454a6
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-21 15:47:55 -0700

    team activity stuff

---
+++ web/TeamActivities/teamActivity.php
@@ -0,0 +1,8 @@
+<?php
+//get post data
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
+$major = htmlspecialchars($_POST['major']);
+//do stuff with the data
+echo "Email: $email<br> Password: $password";
+?>
