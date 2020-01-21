3dd3b59d0663dfceb1ed67f9b46b20f011573ae0
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-21 15:53:06 -0700

    team activity stuff

--- web/TeamActivities/teamActivity.php
+++ web/TeamActivities/teamActivity.php
@@ -3,6 +3,8 @@
 $email    = htmlspecialchars($_POST['email']);
 $password = htmlspecialchars($_POST['password']);
 $major    = htmlspecialchars($_POST['major']);
+$name     = htmlspecialchars($_POST['name']);
+$comments = htmlspecialchars($_POST['comments']);
 //do stuff with the data
 echo "Email: $email<br> Password: $password";
 ?>
