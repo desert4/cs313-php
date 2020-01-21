1eb79611ba70b25b5aba5fce1f9d9e150a3606e3
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-21 15:36:23 -0700

    team activity stuff

--- web/practice/classTempPost.php
+++ web/practice/classTempPost.php
@@ -4,3 +4,8 @@ $password = htmlspecialchars($_POST["passwordForPHP"]);
 
 echo "this is your email $email, and your password is $password";
 ?>
+<html>
+    <body>
+        <button>Checkout?</button>
+    </body>
+</html>
