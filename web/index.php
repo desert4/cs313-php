51a296c293c41b51593e8581340b6da5d12b34f3
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-22 13:08:26 -0700

    fixed nav bug

--- web/index.php
+++ web/index.php
@@ -16,8 +16,8 @@
         <ul class="navigation">
             <li><a class="active" href="index.php">Home</a></li>
             <li><a href="#ABOUT">About</a></li>
-            <li><a href="directory.html" id="directory">Directory</a></li>
             <li><a href="./web/project1/ski.php">Project 1</a></li>
+            <li><a href="directory.html" id="directory">Directory</a></li>
         </ul>
     </nav>
     <div class="grid-container">
