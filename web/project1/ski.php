a1de2311460cac8c3623a15d99fa6645291e5bba
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-22 13:27:49 -0700

    Created nav bar for project 1

--- web/project1/ski.php
+++ web/project1/ski.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <link rel="icon" href="./web/images/Rebel_Logo_logo.jpg">
-    <link href="index1.css" rel="stylesheet" type="text/css">
+    <link href="./web/navigation.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
+    <?php include 'skiNavBar.php' ?>
     <h1>This will be the project 1 homepage</h1>
 </body>
 
---
+++ web/project1/skiNavBar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul class="navigation">
+        <li><a class="active" href="ski.php">Home</a></li>
+        <li><a href="">Reviews</a></li>
+        <li><a href="">Resorts</a></li>
+        <li><a href="">Sign in</a></li>
+    </ul>
+</nav>
