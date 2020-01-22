3afeecc0c28c03918ace5a23e58eabbfafad5f49
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-22 13:13:57 -0700

    fixed nav bar bug

---
+++ web/navigationBar.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul class="navigation">
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="#ABOUT">About</a></li>
+        <li><a href="./web/project1/ski.php">Project 1</a></li>
+        <li><a href="directory.html" id="directory">Directory</a></li>
+    </ul>
+</nav>
