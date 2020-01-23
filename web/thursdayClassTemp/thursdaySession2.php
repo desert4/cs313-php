ea51eb8b971e1e787c1cfcc2e6eb9ae55ffaf28f
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-23 16:17:12 -0700

    Class Activity

--- web/thursdayClassTemp/thursdaySession2.php
+++ web/thursdayClassTemp/thursdaySession2.php
@@ -6,5 +6,11 @@
    $a = $_SESSION['favanimal'];
 
 ?>
-<h1>Your favorite color is <?=$c?> and your favorite animal is <?=$a?> <?php // use the session variables ?></h1>
-<?php ?>
+<h1>Your favorite color is <?=$c?> and your favorite animal is <?=$a?> </h1>
+
+<h3>Stuff</h3>
+<?php // use the session variables 
+if(isset($_SESSION['pictureUrl'])) { ?>
+   <h3>Here is the stuff from the form</h3>
+   <img src="<?=$_SESSION['pictureUrl']?>">
+<?php } ?>
