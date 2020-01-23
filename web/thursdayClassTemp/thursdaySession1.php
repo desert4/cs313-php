7b5730b02533fa17b1a9498b7f158a62d086d3f7
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-23 16:08:25 -0700

    Class Activity

--- web/thursdayClassTemp/thursdaySession1.php
+++ web/thursdayClassTemp/thursdaySession1.php
@@ -1,21 +1,29 @@
 <?php
-   // Start the session
-   session_start();
+// Start the session
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
-   <body>
-      <?php
-         // remove previous session variable
 
-         // Set session variables
-         $_SESSION["favcolor"] = "green";
-         $_SESSION["favanimal"] = "dolphin";
-         // echo that variables have been set
-         echo "Session variables have been set.";
-?>
-      <a href="thursdaySession2.php">Check the variables on another page</a>
+<body>
+   <?php
+   // remove previous session variable
 
-      <?php // set session variables using a form ?>
-   </body>
+   // Set session variables
+   $_SESSION["favcolor"] = "green";
+   $_SESSION["favanimal"] = "dolphin";
+   // echo that variables have been set
+   echo "Session variables have been set.";
+   ?>
+   <a href="thursdaySession2.php">Check the variables on another page</a>
+
+   <h3> lets try this with a form </h3>
+   <form action="" method="POST">
+      <input type="text" name="picture">
+      <input type="submit" name="submit" value="submit!">
+   </form>
+   <?php // set session variables using a form 
+   ?>
+</body>
+
 </html>
