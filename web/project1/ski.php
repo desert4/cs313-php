61907082ec983e7ceb76fa86fe7d9ebf225a2f0e
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-23 21:46:22 -0700

    kelly canyon bug fixes

--- web/project1/ski.php
+++ web/project1/ski.php
@@ -16,7 +16,7 @@
     <h1>Ski Every Day</h1>
     <div class="resort">
         <h3>Kelly Canyon</h3>
-        <a href="http://rocky-reef-99024.herokuapp.com/web/project1/Images/Kelly Canyon Map.jpg"><img src="./web/project1/Images/KellyCanyonMap.jpg" alt="Kelly Canyon"></a>
+        <a href="http://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg"><img src="http://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg" alt="Kelly Canyon"></a>
     </div>
 </body>
 
