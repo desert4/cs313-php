0a6278b775b4183365b07b0c89cebb140ca48060
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-23 22:20:01 -0700

    kelly canyon bug fixes

--- web/project1/ski.php
+++ web/project1/ski.php
@@ -16,20 +16,18 @@
     <h1>Ski Every Day</h1>
     <div class="resort">
         <p>I am in a div</p>
-        <form action="" method="POST" class="resort">
+        <form action="" method="POST">
             <h3>Kelly Canyon</h3>
             <a href="https://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg"><img src="http://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg" alt="Kelly Canyon"></a>
-            <div style="padding: 15px; float: left">
-                <p>Location: Ririe, Idaho</p>
-                <p>Number of runs: </p>
-                <p>Normal Buisiness: light - medium</p>
-                <p>Rating: 3.4</p>
-                <select>
-                    <option value="Half Day">$33 (12:30-5:00 or 5:00-9:00)</option>
-                    <option value="Full Day">$44 (9:30-5:00 or 12:30-9:00)</option>
-                    <option value="Entire Day">$50 (9:30-9:00)</option>
-                </select>
-            </div>
+            <p>Location: Ririe, Idaho</p>
+            <p>Number of runs: </p>
+            <p>Normal Buisiness: light - medium</p>
+            <p>Rating: 3.4</p>
+            <select>
+                <option value="Half Day">$33 (12:30-5:00 or 5:00-9:00)</option>
+                <option value="Full Day">$44 (9:30-5:00 or 12:30-9:00)</option>
+                <option value="Entire Day">$50 (9:30-9:00)</option>
+            </select>
         </form>
     </div>
 </body>
