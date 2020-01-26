c67a4f4575cb73073b713b189690e601ef7df28b
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-25 21:25:43 -0700

    Added post method for kelly

--- web/project1/ski.php
+++ web/project1/ski.php
@@ -19,7 +19,7 @@ session_start();
 <body>
     <?php include 'skiNavBar.php' ?>
     <h1>Ski Every Day</h1>
-    <form action="" method="POST" onclick="updateCart()">
+    <form action="" method="$_POST">
         <div class="resort">
             <h3>Kelly Canyon</h3>
             <a href="https://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg"><img src="http://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg" alt="Kelly Canyon"></a>
@@ -27,12 +27,12 @@ session_start();
             <p>Number of runs: 27</p>
             <p>Normal Buisiness: light - medium</p>
             <p>Rating: 3.4</p>
-            <select>
+            <select  id="Kelly" name="kelly">
                 <option value="Kel1">$33 (Half Day)</option>
                 <option value="Kel2">$44 (3/4 Day)</option>
                 <option value="Kel3">$50 (Full Day)</option>
             </select>
-            <button>ADD TO CART</button>
+            <button type="button" name="KellyButton" value="KellyTicket">ADD TO CART</button>
         </div>
         <div class="resort">
             <h3>Grand Targhee</h3>
@@ -41,7 +41,7 @@ session_start();
             <p>Number of runs: 97</p>
             <p>Normal Buisiness: medium</p>
             <p>Rating: 4.2</p>
-            <select>
+            <select id="Targhee" name="targhee">
                 <option value="Targ1">$79 (weekday)</option>
                 <option value="Targ2">$84 (weekend)</option>
             </select>
@@ -54,16 +54,23 @@ session_start();
             <p>Number of runs: 54</p>
             <p>Normal Buisiness: medium</p>
             <p>Rating: 3.9</p>
-            <select>
-                <option value="pebble1">$52 (Full Day)</option>
-                <option value="pebble2">$41 (Half Day)</option>
-                <option value="pebble3">$22 (Beginner Lift)</option>
-                <option value="pebble4">$208 (5 Day)</option>
+            <select id="Pebble" name="pebble">
+                <option value="Pebble1">$52 (Full Day)</option>
+                <option value="Pebble2">$41 (Half Day)</option>
+                <option value="Pebble3">$22 (Beginner Lift)</option>
+                <option value="Pebble4">$208 (5 Day)</option>
             </select>
             <button>ADD TO CART</button>
         </div>
-
-    </form>
+</form>
 </body>
 
+<?php
+    // CHECK THE BUTTONS
+    if(isset($_POST['KellyButton'])) {
+        $_SESSION['KELLY'] = $_POST['kelly'];
+    }
+    echo '<script> alert("Added item to cart")</script>';
+    print_r($_SESSION)
+?>
 </html>
