21f94e13fd12d01a973428f305e369991b752556
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-25 23:25:40 -0700

    added checkout.php info

--- web/project1/confirmation.php
+++ web/project1/confirmation.php
@@ -18,5 +18,25 @@ session_start();
 
 <body>
     <?php include 'skiNavBar.php' ?>
+
+    <div class=".resort">
+        <h3>Thank You For Your Purchase</h3>
+        <p>Your products:</p>
+        <?php
+            if(isset($_SESSION['KELLY'])) {
+                echo "Kelly Canyon Lift Ticket<br><br>";
+            } 
+            if(isset($_SESSION['TARGHEE'])) {
+                echo "Grand Targhee Lift Ticket<br><br>";
+            } 
+            if(isset($_SESSION['PEBBLE'])) {
+                echo "Pebble Creek Lift Ticket<br><br>";
+            } 
+        ?>
+        <p>Your items will be shipped to:</p>
+        <p><?php 
+        echo htmlspecialchars($_POST["fname"]) . "<br>" . htmlspecialchars($_POST["street"]) . "<br>" . htmlspecialchars($_POST["city"]) . "<br>" . htmlspecialchars($_POST["state"]) . "<br>" . htmlspecialchars($_POST["zip"]);
+        ?>
+    </div>
 </body>
 </html>
