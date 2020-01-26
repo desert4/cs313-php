dcfe4bfa26ba7255a318e15fdd2700319c9a7aee
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-25 22:01:19 -0700

    nav bar bug fixes

---
+++ web/project1/cart.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart</title>
+    <link rel="icon" href="./web/images/Rebel_Logo_logo.jpg">
+    <link href="navigation.css" rel="stylesheet" type="text/css">
+    <link href="homepage.css" rel="stylesheet" type="text/css">
+    <link href="resort.css" rel="stylesheet" type="text/css">
+    <script src="ski.js"></script>
+</head>
+
+<body>
+    <?php include 'skiNavBar.php' ?>
+</body>
