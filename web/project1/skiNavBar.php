33c0fd11225ccfbd5fb822d8835889cddd909ac4
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-25 22:02:58 -0700

    nav bar bug fixes

--- web/project1/skiNavBar.php
+++ web/project1/skiNavBar.php
@@ -1,9 +1,9 @@
 <nav>
     <ul class="navigation">
-        <li><a class="active" href="./web/project1/ski.php">Home</a></li>
+        <li><a class="active" href="ski.php">Home</a></li>
         <li><a href="">Reviews</a></li>
         <li><a href="">Resorts</a></li>
-        <li><a href="./web/project1/cart.php">Cart</a></li>
+        <li><a href="cart.php">Cart</a></li>
         <li><a href="">Sign in</a></li>
     </ul>
 </nav>
