fbef7201e1efdfc924aa0ccac235e06bc0b6c094
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-25 22:42:55 -0700

    cart page updates

--- web/project1/cart.php
+++ web/project1/cart.php
@@ -19,30 +19,65 @@ session_start();
 <body>
     <?php include 'skiNavBar.php' ?>
     <form action="" method="POST">
+        <?php
+        // if (!isset($_SESSION['KELLY']) || !isset($_SESSION['TARGHEE']) || !isset($_POST['PebbleButton'])) {
+        //     echo "There are no itemss on your cart";
+        // } 
+        ?>
+        <?php
+        if (isset($_SESSION["KELLY"])) {
+            echo "Your lift pass to Kelly Canyon is ";
+            if ($_SESSION["KELLY"] == "Kel1") {
+                echo "$33";
+            } elseif ($_SESSION["KELLY"] == "Kel2") {
+                echo "$44";
+            } else {
+                echo "$50";
+            }
+            echo '<input type="submit" name="remove1" value="remove item">';
+        }
+        ?>
+
+        <?php
+        if (isset($_SESSION["TARGHEE"])) {
+            echo "Your lift pass to Grand Targhee is ";
+            if ($_SESSION["KELLY"] == "Targ1") {
+                echo "$79";
+            } else {
+                echo "$84";
+            }
+            echo '<input type="submit" name="remove2" value="remove item">';
+        }
+        ?>
+
+        <?php
+        if (isset($_SESSION["PEBBLE"])) {
+            echo "Your lift pass to Pebble Creek is ";
+            if ($_SESSION["PEBBLE"] == "Pebble1") {
+                echo "$52";
+            } elseif ($_SESSION["PEBBLE"] == "Pebble2") {
+                echo "$41";
+            } elseif ($_SESSION["PEBBLE"] == "Pebble3") {
+                echo "$22";
+            } else {
+                echo "$208";
+            }
+            echo '<input type="submit" name="remove3" value="remove item">';
+        }
+        ?>
+        
+    </form>
     <?php
-    // if (!isset($_SESSION['KELLY']) || !isset($_SESSION['TARGHEE']) || !isset($_POST['PebbleButton'])) {
-    //     echo "There are no itemss on your cart";
-    // } 
-    ?>
-    <?php 
-    if (isset($_SESSION["KELLY"])) {
-        echo "Your lift pass to Kelly Canyon is ";
-        if($_SESSION["KELLY"] == "Kel1") {
-            echo "$33";
-        }
-        elseif ($_SESSION["KELLY"]) {
-            echo "$44";
-        }
-        else {
-            echo "$50";
-        }
-        echo '<input type="submit" name="remove1" value="remove item">';
-    }
-    ?>
-        </form>
-    <?php 
     if (isset($_POST["remove1"])) {
         unset($_SESSION["KELLY"]);
+    }
+
+    if (isset($_POST["remove2"])) {
+        unset($_SESSION["TARGHEE"]);
+    }
+
+    if (isset($_POST["remove3"])) {
+        unset($_SESSION["PEBBLE"]);
     }
     ?>
 </body>
