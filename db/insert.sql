9f6312056c62deb91f363e3aaad8d2eb4820b56b
desert4 <48767694+desert4@users.noreply.github.com>
2020-01-31 12:10:07 -0700

    added insert.sql file

---
+++ db/insert.sql
@@ -0,0 +1,14 @@
+-- ----------------------------------------------- --
+-- ---------   POPULATE THE USER TABLE   --------- --
+-- ----------------------------------------------- --
+INSERT INTO public.resort ( name
+                        , city
+                        , state
+                        , cost
+                        , rating)
+                VALUES  ( 'Kelly Canyon'
+                        , 'Ririe'
+                        , 'Idaho'
+                        , 40
+                        , 3.4
+                        );
--- db/myDb.sql
+++ db/myDb.sql
@@ -1,22 +1,23 @@
-CREATE TABLE user
+CREATE TABLE public.user
 ( id            SERIAL          NOT NULL PRIMARY KEY
 , username      VARCHAR(100)    NOT NULL UNIQUE
 , password      VARCHAR(100)    NOT NULL
 );
 
-CREATE TABLE resort
+CREATE TABLE public.resort
 ( id            SERIAL          NOT NULL PRIMARY KEY
 , name          VARCHAR(100)    NOT NULL
 , city          VARCHAR(100)    NOT NULL
 , state         VARCHAR(100)    NOT NULL
 , cost          INT             
-, rating        REAL            NOT NULL REFERENCES review(rating)
+, rating        REAL            NOT NULL
 );
 
-CREATE TABLE review
+CREATE TABLE public.review
 ( id            SERIAL          NOT NULL PRIMARY KEY
-, place         VARCHAR(100)    NOT NULL REFERENCES resort(id)
-, reviewer      VARCHAR(100)    NOT NULL REFERENCES user(username)
+, place         INT             NOT NULL REFERENCES resort(id)
+, person        VARCHAR(100)    NOT NULL REFERENCES public.user(username)
 , date          DATE            NOT NULL
 , rating        REAL            NOT NULL
 );
+
