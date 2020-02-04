11e19300be3cd0eeb0a57f9939926b21f30b3c60
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-04 16:21:01 -0700

    Tuesday team activity

--- web/practice/indexPractice.php
+++ web/practice/indexPractice.php
@@ -2,15 +2,16 @@
     require "dbConnect.php";
     $db = get_db();
 
-    $family_members = $db->prepare("SELECT * FROM w5_family_members");
-    $family_members->execute();
+    $scriptures = $db->prepare("SELECT * FROM Scriptures");
+    $scriptures->execute();
 
-    while ($fRow = $family_members->fetch(PDO::FETCH_ASSOC))
+    while ($fRow = $scriptures->fetch(PDO::FETCH_ASSOC))
     {
-        $first_name = $fRow["first_name"];
-        $last_name = $fRow["last_name"];
-        $relationship_id = $fRow["relationship_id"];
+        $book = $fRow["book"];
+        $chapter = $fRow["chapter"];
+        $verse = $fRow["verse"];
+        $content = $fRow["content"];
 
-        echo "<p>$first_name $last_name is my $relationship_id</p>";
+        echo "<p><b>$book $chapter:$verse - </b>$content</p>";
     }
 ?>
--- web/practice/practice.sql
+++ web/practice/practice.sql
@@ -1,34 +1,13 @@
-DROP TABLE IF EXISTS w5_family_members;
-DROP TABLE IF EXISTS w5_relationships;
-CREATE TABLE w5_relationships
+DROP TABLE IF EXISTS Scriptures;
+
+CREATE TABLE Scriptures
 ( id          SERIAL       NOT NULL PRIMARY KEY
-, description VARCHAR(100) NOT NULL);
-CREATE TABLE w5_family_members
-( id              SERIAL        NOT NULL PRIMARY KEY
-, first_name      VARCHAR(100)  NOT NULL
-, last_name       VARCHAR(100)  NOT NULL
-, relationship_id INT           NOT NULL REFERENCES w5_relationships(id));
-INSERT INTO w5_relationships (description) VALUES ('Mother');
-INSERT INTO w5_relationships (description) VALUES ('Father');
-INSERT INTO w5_relationships (description) VALUES ('Wife');
-INSERT INTO w5_relationships (description) VALUES ('Husband');
-INSERT INTO w5_relationships (description) VALUES ('Son');
-INSERT INTO w5_relationships (description) VALUES ('Daughter');
-INSERT INTO w5_family_members ( first_name
-                              , last_name
-                              , relationship_id)
-                       VALUES ( 'Sarah'
-                              , 'Birch'
-                              , 3);
-INSERT INTO w5_family_members ( first_name
-                              , last_name
-                              , relationship_id)
-                       VALUES ( 'Teresa'
-                              , 'Birch'
-                              , 1);
-INSERT INTO w5_family_members ( first_name
-                              , last_name
-                              , relationship_id)
-                       VALUES ( 'Hannah'
-                              , 'Birch'
-                              , 6);
+, book        VARCHAR(100) NOT NULL
+, chapter     INT          NOT NULL
+, verse       INT          NOT NULL
+, content     VARCHAR(250) NOT NULL);
+
+INSERT INTO Scriptures (book, chapter, verse, content) VALUES ('John', 1, 5, 'And the light shineth in darkness; and the darkness ccomprehended it not.');
+INSERT INTO Scriptures (book, chapter, verse, content) VALUES ('Doctrine and Covenants', 88, 49, 'The alight shineth in darkness, and the darkness comprehendeth it not; nevertheless, the day shall come when you shall bcomprehend even God, being quickened in him and by him.');
+INSERT INTO Scriptures (book, chapter, verse, content) VALUES ('Doctrine and Covenants', 93, 28, 'He that akeepeth his commandments receiveth btruth and clight, until he is glorified in truth and dknoweth all things.');
+INSERT INTO Scriptures (book, chapter, verse, content) VALUES ('Mosiah', 16, 9, 'He is the alight and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
