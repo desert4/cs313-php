f404496dbaf5583ed0060779f994d8d7b73175b7
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-05 10:43:06 -0700

    removed shopping features

--- web/project1/ski.php
+++ web/project1/ski.php
@@ -19,7 +19,7 @@ session_start();
 <body>
     <?php include 'skiNavBar.php' ?>
     <h1>Ski Every Day</h1>
-    <form action="" method="POST">
+    <!-- <form action="" method="POST"> -->
         <div class="resort">
             <h3>Kelly Canyon</h3>
             <a href="https://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg"><img src="http://rocky-reef-99024.herokuapp.com/project1/Images/KellyCanyonMap.jpg" alt="Kelly Canyon"></a>
@@ -27,12 +27,12 @@ session_start();
             <p>Number of runs: 27</p>
             <p>Normal Buisiness: light - medium</p>
             <p>Rating: 3.4</p>
-            <select  id="Kelly" name="kelly">
+            <!-- <select  id="Kelly" name="kelly">
                 <option value="Kel1">$33 (Half Day)</option>
                 <option value="Kel2">$44 (3/4 Day)</option>
                 <option value="Kel3">$50 (Full Day)</option>
             </select>
-            <input type="submit" name="KellyButton" value="Add To Cart">
+            <input type="submit" name="KellyButton" value="Add To Cart"> -->
         </div>
         <div class="resort">
             <h3>Grand Targhee</h3>
@@ -41,11 +41,11 @@ session_start();
             <p>Number of runs: 97</p>
             <p>Normal Buisiness: medium</p>
             <p>Rating: 4.2</p>
-            <select id="Targhee" name="targhee">
+            <!-- <select id="Targhee" name="targhee">
                 <option value="Targ1">$79 (weekday)</option>
                 <option value="Targ2">$84 (weekend)</option>
             </select>
-            <input type="submit" name="TargheeButton" value="Add To Cart">
+            <input type="submit" name="TargheeButton" value="Add To Cart"> -->
         </div>
         <div class="resort">
             <h3>Pebble Creek</h3>
@@ -54,15 +54,15 @@ session_start();
             <p>Number of runs: 54</p>
             <p>Normal Buisiness: medium</p>
             <p>Rating: 3.9</p>
-            <select id="Pebble" name="pebble">
+            <!-- <select id="Pebble" name="pebble">
                 <option value="Pebble1">$52 (Full Day)</option>
                 <option value="Pebble2">$41 (Half Day)</option>
                 <option value="Pebble3">$22 (Beginner Lift)</option>
                 <option value="Pebble4">$208 (5 Day)</option>
             </select>
-            <input type="submit" name="PebbleButton" value="Add To Cart">
+            <input type="submit" name="PebbleButton" value="Add To Cart"> -->
         </div>
-</form>
+<!-- </form> -->
 </body>
 
 <?php
