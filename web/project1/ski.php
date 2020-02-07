c586f4e46f7d364082d45efab833d576b2afb163
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-07 13:11:19 -0700

    ski.php bug fixes

--- web/project1/ski.php
+++ web/project1/ski.php
@@ -12,7 +12,7 @@ $db = get_db();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="icon" href="./web/project1/Images/skier.png">
+    <link rel="icon" href="./Images/skier.png">
     <link href="navigation.css" rel="stylesheet" type="text/css">
     <link href="homepage.css" rel="stylesheet" type="text/css">
     <link href="resort.css" rel="stylesheet" type="text/css">
