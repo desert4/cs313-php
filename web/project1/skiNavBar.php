2caca9d627cb7c66e14099f62c007a3df7d8abec
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-07 12:40:13 -0700

    Navbar bug fix

--- web/project1/skiNavBar.php
+++ web/project1/skiNavBar.php
@@ -4,6 +4,6 @@
         <li><a href="">Reviews</a></li>
         <li><a href="">Resorts</a></li>
         <!-- <li><a href="cart.php">Cart</a></li> -->
-        <li><a href="">Sign in</a></li>
+        <li><a href="signIn.php">Sign in</a></li>
     </ul>
 </nav>
