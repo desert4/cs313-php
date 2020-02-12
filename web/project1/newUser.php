860826cbd10e886be42e8079ac698e4a985e1401
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-12 10:59:20 -0700

    new user page fixes

--- web/project1/newUser.php
+++ web/project1/newUser.php
@@ -19,12 +19,15 @@ $db = get_db();
 
 <body>
     <?php include 'skiNavBar.php' ?>
-    <form action="ski.php" method="POST">
-        <label>Please enter the username you want: </label>
-        <input type="text" placeholder="username"><br><br>
-        <label>Please enter your password: </label>
-        <input type="password" placeholder="password"><br><br>
-        <label>Please enter your email: </label>
-        <input type="text" placeholder="password"><br><br>
-    </form>
+    <div class="resort">
+        <form action="ski.php" method="POST">
+            <label>Please enter the username you want: </label>
+            <input type="text" placeholder="username"><br><br>
+            <label>Please enter your password: </label>
+            <input type="password" placeholder="password"><br><br>
+            <label>Please enter your email: </label>
+            <input type="text" placeholder="password"><br><br>
+            <input type="submit" value="Create">
+        </form>
+    </div>
 </body>
