396c2ab470567680ab756d7160bfe8e6c95b33db
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-12 11:12:59 -0700

    fixed search bugs

--- web/project1/skiNavBar.php
+++ web/project1/skiNavBar.php
@@ -1,7 +1,7 @@
 <nav>
     <ul class="navigation">
         <li><a class="active" href="ski.php">Home</a></li>
-        <li><input type="text" placeholder="Search..." name="search" id="search"></li>
+        <li><form action="ski.php" method="POST"><input type="text" placeholder="Search..." name="search" id="search"></form></li>
         <!-- <li><a href="">Reviews</a></li> -->
         <!-- <li><a href="">Resorts</a></li> -->
         <!-- <li><a href="cart.php">Cart</a></li> -->
