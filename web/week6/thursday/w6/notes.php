54fcde436808393675c7b57e376f6e8ff3a520b6
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-13 16:29:07 -0700

    Thusrady activity changes

--- web/week6/thursday/w6/notes.php
+++ web/week6/thursday/w6/notes.php
@@ -17,11 +17,49 @@
          <?php
 				// get and display course header
 					// this should include all course information (outside of the id)
-            
-
+				$course_id = $_GET['courseId'];
+				$statement = $db->prepare("SELECT * FROM w6_course WHERE id=$course_id");
+				$statement->execute();
+				while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+					$id = $row['id'];
+					$name = $row['name'];
+					$number = $row['number'];
+					echo "<h2>$name</h2>";
+					echo "<p>Course ID: $id";
+					echo "<p>Course number: $number";
+				}
+				
 				// get and display notes for course
-					// this should include all note information, including the name of the individual that wrote the note
+				// this should include all note information, including the name of the individual that wrote the note
+				$statement = $db->prepare("SELECT * FROM w6_note");
+				$statement->execute();
+				while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+					$id = $row['id'];
+					$course = $row['course_id'];
+					$student = $row['student_id'];
+					$title = $row['title'];
+					$content = $row['content'];
+					$date = $row['date'];
+					$time = $row['time'];
 
+					// get the name of the student
+					$statement2 = $db->prepare("SELECT * FROM w6_student WHERE id=$student");
+					$statement2->execute();
+					while ($sRow = $statement2->fetch(PDO::FETCH_ASSOC)) {
+						$student_id = $sRow['id'];
+						$student_name = $sRow['name'];
+
+						echo "<h3>$student_name</h3>";
+					}
+
+					echo "<p>Not ID: $id";
+					echo "<p>Course ID: $course";
+					echo "<p>Student: $student";
+					echo "<p>Title: $title";
+					echo "<p>Content: $content";
+					echo "<p>date: $date";
+					echo "<p>time: $time";
+				}
 				// this should look professional
             
          ?>
