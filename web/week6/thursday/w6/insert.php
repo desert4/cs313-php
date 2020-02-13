114537fba7d9b90d8da2f74366cb1b70b11acc20
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-13 16:08:46 -0700

    thursday class materials

--- web/week6/thursday/w6/insert.php
+++ web/week6/thursday/w6/insert.php
@@ -1,5 +1,9 @@
 <?php
 // retrieve variables from form
+$student_name = $_POST['student_name'];
+$note_title = $_POST['noteTitle'];
+$course_note = $_POST['courseNote'];
+$student_class = $_POST['student_class'];
 
 // get time
 date_default_timezone_set("America/Denver");
@@ -8,9 +12,13 @@ $time = date("h:i:sa");
 require("dbConnect.php");
 $db = get_db();
 
+// get date
+$date = date("y-m-d");
 try
 {
-   // insert into note table
+	// insert into note table
+	$query = "INSERT INTO w6_note(course_id, student_id, title, content, date, time)
+	VALUES($student_class, $student_name, $note_title, $course_note, $date, $time)";
 }
 catch (Exception $ex)
 {
@@ -19,5 +27,4 @@ catch (Exception $ex)
 }
 header("Location: notes.php/?courseId=$course");
 
-die(); 
-?>
+die();
