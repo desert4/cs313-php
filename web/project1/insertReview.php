ae322428831052a1ff53326a4f00a5d7ca127180
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-14 19:10:41 -0700

    added ability to upload a review

--- web/project1/insertReview.php
+++ web/project1/insertReview.php
@@ -1,3 +1,25 @@
 <?php
+session_start();
+$resort = $_POST["resort"];
+$rating = $_POST["rating"];
+$user = $_POST["user"];
 
+require("dbConnect.php");
+$db = get_db();
+try {
+    $query = "INSERT INTO review(place, reviewer, rating) VALUES(:resort, :rating, :user)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':resort', $resort);
+    $statement->bindValue('rating', $rating);
+    $statement->bindValue(':user', $user);
+    $statement->execute();
+
+    header('Refresh: 2, URL=https://rocky-reef-99024.herokuapp.com/project1/ski.php');
+}
+catch (Exception $ex) {
+    echo "ERROR with the DB. Details:<br>$ex";
+    die();
+}
+
+die();
 ?>
