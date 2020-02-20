ea15b9cfa087f7b4abf503146da24da3f90bd7d1
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-20 16:10:31 -0700

    password bug fix

--- web/project1/signingIn.php
+++ web/project1/signingIn.php
@@ -5,20 +5,23 @@ session_start();
 // connect to the database
 require("dbConnect.php");
 $db = get_db();
+echo "<h1>connected to the databse</h1>";
 
 // get the information entered on the previous page
 $username = $_POST["username"];
 $password = $_POST["password"];
-echo "<h1>this page loaded</h1>";
+echo "<h1>posting the data</h1>";
 
 // check the database for the user
 $queryCheck = "SELECT COUNT(*) FROM person WHERE username='$username' AND password='";
 $check = $db->prepare($queryCheck);
 $check->execute();
+echo "<h1>querying the data</h1>";
 
 
 // if found redirect to the home page
 if($check == 1) {
+    echo "<h1>found the user</h1>";
 //     // get the hashed password
 //     $passwordQuery = "SELECT * FROM person WHERE username='$username'";
 //     $getPass = $db->prepare("$passwordQuery");
@@ -42,6 +45,7 @@ if($check == 1) {
 // }
 // // if not in the database redirect back to sign in page with error
 else {
+    echo "<h1>did not find the user</h1>";
     header( "Location: https://rocky-reef-99024.herokuapp.com/project1/signIn.php/?type=invalidname");
 }
 
