a4c9ae095f5c3a2904fa3f1e9f8efc0b478239a2
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-20 16:03:01 -0700

    password bug fix

--- web/project1/signingIn.php
+++ web/project1/signingIn.php
@@ -2,14 +2,14 @@
 session_start();
 
 
-echo "<h1>this page loaded</h1>";
-// // connect to the database
-// require("dbConnect.php");
-// $db = get_db();
+// connect to the database
+require("dbConnect.php");
+$db = get_db();
 
-// // get the information entered on the previous page
-// $username = $_POST["username"];
-// $password = $_POST["password"];
+// get the information entered on the previous page
+$username = $_POST["username"];
+$password = $_POST["password"];
+echo "<h1>this page loaded</h1>";
 
 // // check the database for the user
 // $queryCheck = "SELECT COUNT(*) FROM person WHERE username='$username' AND password='";
