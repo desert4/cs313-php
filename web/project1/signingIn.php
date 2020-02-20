7315c225475e409368372ea5c85a5b3f1e5720ba
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-20 16:04:47 -0700

    password bug fix

--- web/project1/signingIn.php
+++ web/project1/signingIn.php
@@ -11,16 +11,16 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 echo "<h1>this page loaded</h1>";
 
-// // check the database for the user
-// $queryCheck = "SELECT COUNT(*) FROM person WHERE username='$username' AND password='";
-// $check = $db->prepare($queryCheck);
-// $check->execute();
+// check the database for the user
+$queryCheck = "SELECT COUNT(*) FROM person WHERE username='$username' AND password='";
+$check = $db->prepare($queryCheck);
+$check->execute();
 
 
-// // if found redirect to the home page
-// if($check == 1) {
+// if found redirect to the home page
+if($check == 1) {
 //     // get the hashed password
-//     $passwordQuery = "SELECT * FROM review WHERE place='$username'";
+//     $passwordQuery = "SELECT * FROM person WHERE username='$username'";
 //     $getPass = $db->prepare("$passwordQuery");
 //     $getPass->execute();
 
@@ -31,12 +31,12 @@ echo "<h1>this page loaded</h1>";
 
 //     // // check the password
 //     // if(password_verify($password, $hashedPassword)) {
-//         $_SESSION['name'] = $username;
-//         header( "Location: https://rocky-reef-99024.herokuapp.com/project1/ski.php");    
-//     // } else {
-//     //     header( "Location: https://rocky-reef-99024.herokuapp.com/project1/signIn.php/?type=invalidpassword");
+        $_SESSION['name'] = $username;
+        header( "Location: https://rocky-reef-99024.herokuapp.com/project1/ski.php");    
+    } else {
+        header( "Location: https://rocky-reef-99024.herokuapp.com/project1/signIn.php/?type=invalidpassword");
 
-//     // }
+    }
 //     die();
 // }
 // // if not in the database redirect back to sign in page with error
