adcd4515ccd212b7a46452529c945885144d555e
desert4 <48767694+desert4@users.noreply.github.com>
2020-02-22 12:17:54 -0700

    new user bug fix

--- web/project1/createUser.php
+++ web/project1/createUser.php
@@ -14,35 +14,52 @@ $db = get_db();
 $queryCheck = "SELECT COUNT(*) FROM person WHERE username='$username'";
 $check = $db->prepare($queryCheck);
 $check->execute();
+?>
+<html>
 
-if($check == 1) {
-    header( "Location: https://rocky-reef-99024.herokuapp.com/project1/newUser.php?type=invalid");
-}
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New User</title>
+    <link rel="icon" href="./Images/skier.png">
+    <link href="navigation.css" rel="stylesheet" type="text/css">
+    <link href="homepage.css" rel="stylesheet" type="text/css">
+    <link href="resort.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <?php
+    if ($check == 1) {
+        echo "<div class='resort'><p>The username that you have entered, $username, has already been taken.  Please click the button to return to the previous page to pick a different username.";
+        echo "<form action='./newUser.php' method='POST'>";
+        echo  "<input type='submit' value='Go Back' name='submit'</form></div>";
+    } else {
+        try {
+            // hash the password
+            $passwordHash = password_hash("$password", PASSWORD_DEFAULT);
+
+            // store the user
+            $query = "INSERT INTO person(username, password, email) VALUES (:username, :password, :email)";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':username', $username);
+            $statement->bindValue(':password', $passwordHash);
+            $statement->bindValue(':email', $email);
+            $statement->execute();
+        } catch (Exception $ex) {
+            echo "Error with DB. Details: $ex";
+            die();
+        }
+
+        // set the session username
+        $_SESSION['name'] = $username;
+    }
+
+    header('Refresh: 2; URL=https://rocky-reef-99024.herokuapp.com/project1/ski.php');
+    die();
+    ?>
+</body>
+
+</html>
+<?php
 // if it doesnt exist, create the user
-else {
-    try
-    {
-        // hash the password
-        $passwordHash = password_hash("$password", PASSWORD_DEFAULT);
-        
-        // store the user
-        $query = "INSERT INTO person(username, password, email) VALUES (:username, :password, :email)";
-        $statement = $db->prepare($query);
-        $statement->bindValue(':username', $username);
-        $statement->bindValue(':password', $passwordHash);
-        $statement->bindValue(':email', $email);
-        $statement->execute();
-    }
-    catch (Exception $ex)
-    {
-        echo "Error with DB. Details: $ex";
-        die();
-    }
-
-    // set the session username
-    $_SESSION['name'] = $username;
-}
-
-header('Refresh: 2; URL=https://rocky-reef-99024.herokuapp.com/project1/ski.php');
-die();
 ?>
--- web/project1/newUser.php
+++ web/project1/newUser.php
@@ -15,7 +15,7 @@ $invalid = $_GET['type'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>New User</title>
     <link rel="icon" href="./Images/skier.png">
     <link href="navigation.css" rel="stylesheet" type="text/css">
     <link href="homepage.css" rel="stylesheet" type="text/css">
