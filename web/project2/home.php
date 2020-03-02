2717bfe2e1a49884f63f12318cc1c7d442fd8369
desert4 <48767694+desert4@users.noreply.github.com>
2020-03-02 14:43:46 -0700

    added a temp query

--- web/project2/home.php
+++ web/project2/home.php
@@ -2,8 +2,8 @@
 session_start();
 
 // conection to the database
-// require "dbConnect.php";
-// $db = get_db();
+require "dbConnect.php";
+$db = get_db();
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,7 +17,24 @@ session_start();
     <link href="homepage.css" rel="stylesheet" type="text/css">
     <link href="resort.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
     <p>this is the home page</p>
+    <?php
+    // get the query
+        $query = "SELECT * FROM job";
+        $job = $db->prepare("$query");
+        $job->execute();
+
+        while ($row = $job->fetch(PDO::FETCH_ASSOC)) {
+            $id = $row["id"];
+            $name = $row["name"];
+            $cost = $row["cost"];
+            $time = $row["time_estimate"];
+
+            echo "<h1>The job id is $id and the job type is a $name.  The cost is $cost and it should take $time days.";
+        }
+    ?>
 </body>
+
 </html>
